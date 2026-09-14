de3d09c8d1f1d4dcdabf8c61f9308402c517c518
royall1510s-pixel <royall1510s@gmail.com>
2026-09-14 11:44:49 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,20 @@
-plugins {
-    id("com.android.application") version "8.1.0" apply false
-    kotlin("android") version "1.9.10" apply false
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Space Frontier"
+
+include(":app") 
