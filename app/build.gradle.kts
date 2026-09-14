d83f09740bbaedda3c2bb3cb0394a18b06731db8
royall1510s-pixel <royall1510s@gmail.com>
2026-09-14 12:30:23 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,12 +15,17 @@ android {
         versionName = "1.0"
     }
 
-    buildFeatures {
-        viewBinding = true
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
         jvmTarget = "17"
+    }
+
+    buildFeatures {
+        viewBinding = true
     }
 }
 
