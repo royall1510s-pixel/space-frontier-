8f407f647bd47b34704cd4385f18f8f6461023ea
royall1510s-pixel <royall1510s@gmail.com>
2026-09-14 14:27:42 +0200

    Update GameState.kt

--- app/src/main/kotlin/com/spacefrontier/game/models/GameState.kt
+++ app/src/main/kotlin/com/spacefrontier/game/models/GameState.kt
@@ -1,20 +1,29 @@
 package com.spacefrontier.game.models
 
 data class GameState(
-    val rocket: Rocket = Rocket(),
+    var rocket: Rocket = Rocket(),
     var currentPlanet: Planet? = null,
     var totalCoins: Int = 0,
-    var gamePhase: GamePhase = GamePhase.AWAITING_FIRST_TAP
+    var gamePhase: GamePhase =
+        GamePhase.AWAITING_FIRST_TAP
 ) {
 
     enum class GamePhase {
+
         AWAITING_FIRST_TAP,
+
         STAGE_1_ACTIVE,
+
         STAGE_2_ACTIVE,
+
         STAGE_3_ACTIVE,
+
         IN_FLIGHT,
+
         LANDING_SEQUENCE,
+
         LANDED_SUCCESS,
+
         LANDED_FAILED
     }
-}
+} 
