69a4ee9c321592211a27b31dd4b79629338bc066
royall1510s-pixel <royall1510s@gmail.com>
2026-09-14 10:43:21 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,5 @@
-plugins {
-    id("com.android.application")
-    kotlin("android")
-}
-
-android {
-    namespace = "com.spacefrontier.game"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.spacefrontier.game"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
-    }
-
-    buildFeatures {
-        viewBinding = true
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-}
-
-dependencies {
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.android.material:material:1.10.0")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.10")
-}
+org.gradle.jvmargs=-Xmx2048m -XX:MaxPermSize=512m
+org.gradle.parallel=true
+kotlin.code.style=official
+android.useAndroidX=true
+android.enableJetifier=true
